48c36c971b95e08042322e67e0d18f8a14a1f954
Deep Khajanchi <deepkhajanchi@Deeps-MacBook-Pro.local>
2023-12-19 16:10:22 -0800

    Designed dynamic array

--- Basics/Design Problems/Design Dynamic Array/DynamicArray.java
+++ Basics/Design Problems/Design Dynamic Array/DynamicArray.java
@@ -1,21 +1,13 @@
-/**
- * Thought process:
- * - Initialize an array of an infinite size. Idea is to add/remove number of elements to/from the array
- * and request should be fulfiled.
- *
- *
- */
-
 class DynamicArray{
     private int arr[];
     private int capacity;
     private int length;
 
     //constructor to initialize the dynamic array
-    public DynamicArray(int capacity){
-        this.capacity= capacity;
-        this.length= 0;
-        this.arr= new int[this.capacity];
+    public DynamicArray(){
+        capacity= 1;
+        length= 0;
+        arr= new int[1];
     }
 
     //gets element from given index
@@ -24,7 +16,7 @@ class DynamicArray{
     }
 
     //adds element to given index
-    public int add(int index, int element){
+    public void addAt(int index, int element){
         if(length == capacity)  growSize();
 
         for(int i= length-1; i>=index; i--){
@@ -32,26 +24,24 @@ class DynamicArray{
         }
         arr[index]= element;
         length++;
-        return arr[index];
     }
 
     //adds at the last index of arr.
-    public int addLast(int element){
+    public void addLast(int element){
         if(length == capacity){
             growSize();
         }
         arr[length]= element;
         length++;
-        return arr.length;
     }
 
     //removes the last element from arr.
-    public int remove(int index){
+    public void removeLast(){
         if(length > 0)  length--;
-        return arr.length;
     }
 
-    public int removeAt(int index){
+    //removes the last element from given index.
+    public void removeAt(int index){
         if(length > 0){
             for(int i=index; i<length-1; i++){
                 arr[i]= arr[i+1];
@@ -59,18 +49,19 @@ class DynamicArray{
             arr[length-1]= 0;
             length--;
         }
-        return arr.length;
     }
 
+    //increses the capacity of arr to double
     private void growSize(){
         capacity *= 2;
         int newArr[]= new int[capacity];
-        for(int i=0; i< newArr.length; i++){
+        for(int i=0; i< length; i++){
             newArr[i]= arr[i];
         }
         arr= newArr;
     }
 
+    //decreases the capacity of the arr to the current length
     public void shrinkSize(){
         if(length > 0){
             int newArr[] = new int[length];
@@ -82,19 +73,55 @@ class DynamicArray{
         }
     }
 
+    //get arr's current size till where it is filled
     public int getSize(){
         return length;
     }
 
+    //get arr's current capapcity to add elements
     public int getCapacity(){
         return capacity;
     }
 
-    public static void main(String[] args) {
-        DynamicArray dynamicArray= new DynamicArray(5);
-        System.out.println(dynamicArray.getCapacity());
-        System.out.println("Element at given index: " + dynamicArray.add(2,44));
-        System.out.println(dynamicArray.getSize());
+    public static void main(String[] args) throws Exception{
+        DynamicArray dynamicArray= new DynamicArray();
+
+        dynamicArray.addLast(1);
+        dynamicArray.addLast(2);
+        dynamicArray.addLast(3);
+        dynamicArray.addLast(4);
+        dynamicArray.addLast(5);
+        dynamicArray.addLast(6);
+        dynamicArray.addLast(7);
+
+        System.out.println("Elements of array:");
+        for(int i=0; i< dynamicArray.capacity; i++){
+            System.out.print(dynamicArray.arr[i] + " ");
+        }
+
+        System.out.println("Size of the array: "+ dynamicArray.getCapacity());
+        System.out.println("Number of element in the array: "+dynamicArray.getSize());
+
+        dynamicArray.shrinkSize();
+        System.out.println("Elements of array after shrinSize of array:");
+        for(int i=0; i< dynamicArray.capacity; i++){
+            System.out.print(dynamicArray.arr[i] + " ");
+        }
+
+        dynamicArray.addAt(1,23);
+        System.out.println("Elements of array after adding at index 1:");
+        for(int i=0; i< dynamicArray.capacity; i++){
+            System.out.print(dynamicArray.arr[i] + " ");
+        }
+
+        dynamicArray.removeLast();
+        System.out.println("Elements of array after removing the last element:");
+        for(int i=0; i< dynamicArray.capacity; i++){
+            System.out.print(dynamicArray.arr[i] + " ");
+        }
+
+        dynamicArray.removeAt(1);
+        System.out.println("Elements of array after removing the element at index 1:");
         for(int i=0; i< dynamicArray.capacity; i++){
             System.out.print(dynamicArray.arr[i] + " ");
         }
