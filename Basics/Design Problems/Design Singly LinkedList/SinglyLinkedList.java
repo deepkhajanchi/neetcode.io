9e6078822269cdd9b5b8f9669680f776bd686873
Deep Khajanchi <deepkhajanchi@Deeps-MacBook-Pro.local>
2023-12-27 20:58:55 -0800

    Singly LL refactored

--- Basics/Design Problems/Design Singly LinkedList/SinglyLinkedList.java
+++ Basics/Design Problems/Design Singly LinkedList/SinglyLinkedList.java
@@ -2,7 +2,9 @@ class Node{
     int value;
     Node next;
     public Node(int value){
+
         this.value= value;
+
     }
 }
 
@@ -22,8 +24,10 @@ public class SinglyLinkedList{
         for(int i= 0; i< index; i++){
             cur= cur.next;
         }
+        System.out.println("At "+index+" position, node value "+ cur.value);
         return cur.value;
     }
+
     public void addAtIndex(int index, int value){
         Node cur= new Node(value);
         if(index> size)    return;
@@ -34,17 +38,28 @@ public class SinglyLinkedList{
             tail= cur;
         }else{
             Node prev= head;
-            for(int i= 0; i< index-1; i++){
+            for(int i=0; i<index-1; i++){
                 prev= prev.next;
             }
             cur.next= prev.next;
             prev.next= cur;
         }
         size++;
-        return;
+        System.out.println("Added node: "+ cur.value+" at index: "+ index);
     }
+
     public void addAtHead(int value){
-        addAtIndex(0, value);
+
+        Node cur= new Node(value);
+        if(head == null){
+            head= cur;
+            tail= cur;
+        }else{
+            cur.next= head;
+            head= cur;
+        }
+        size++;
+
     }
     public void addAtTail(int value){
         addAtIndex(size, value);
@@ -52,12 +67,18 @@ public class SinglyLinkedList{
 
     public void deleteAtIndex(int index) {
         if(index< 0 || index>= size)    return;
-        Node prev= head;
-        for(int i= 0; i< index; i++){
-            prev= prev.next;
+        if(index == 0)  head= head.next;
+        else {
+            Node prev = head;
+            Node temp= null;
+            for (int i = 0; i < index-1; i++) {
+                prev = prev.next;
+            }
+            temp= prev.next;
+            prev.next = temp.next;
+            size--;
+            System.out.println("Deleted Node: "+ temp.value+" at index: "+ index);
         }
-        prev.next= prev.next.next;
-        size--;
     }
 
     public void showLinkedList(){
@@ -76,14 +97,17 @@ public class SinglyLinkedList{
         System.out.print("null");
         System.out.println();
     }
+
     public static void main(String[] args) {
         SinglyLinkedList ll= new SinglyLinkedList();
-        System.out.println(ll);
         ll.addAtIndex(0,22);
         ll.addAtIndex(1,23);
         ll.showLinkedList();
         System.out.println(ll.get(0));
-        ll.deleteAtIndex(1);
-        System.out.println(ll.get(1));
+       // ll.deleteAtIndex(1);
+        ll.addAtHead(20);
+        ll.showLinkedList();
+        ll.addAtTail(21);
+        ll.showLinkedList();
     }
 }
